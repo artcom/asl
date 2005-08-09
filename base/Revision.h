f980e77c26e8d3232e40636d3b1baf5c4e690841
jens <jens@f3027e6b-e3f5-0310-a8e1-edde99107618>
2005-08-09 16:45:29 +0000

    - more 'revision' stuff - Makedist.pl now checks only the dependencies - watchdog, metex now also have a --revision
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@1538 f3027e6b-e3f5-0310-a8e1-edde99107618

---
+++ base/Revision.h
@@ -0,0 +1,20 @@
+//=============================================================================
+// Copyright (C) 2005, ART+COM AG Berlin
+//
+// These coded instructions, statements, and computer programs contain
+// unpublished proprietary information of ART+COM AG Berlin, and
+// are copy protected by law. They may not be disclosed to third parties
+// or copied or duplicated in any form, in whole or in part, without the
+// specific, prior written permission of ART+COM AG Berlin.
+//=============================================================================
+
+#ifndef AC_ASL_REVISION_INCLUDED
+#define AC_ASL_REVISION_INCLUDED
+
+#include <string>
+
+namespace asl {
+    extern std::string ourRevision;
+}
+
+#endif
